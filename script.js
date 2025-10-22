1431352b6458e099adc560eeb0d099ba8427967d
dhanalakshmikonar <konardhanalakshmi@gmail.com>
2025-10-22 16:55:52 +0530

    files added

--- script.js
+++ script.js
@@ -7,3 +7,19 @@ function nextSlide() {
   document.querySelector(".slides").style.transform = `translateX(-${currentSlide * 100}%)`;
 }
 
+const viewMoreLink = document.getElementById('viewMoreLink');
+const diseaseList = document.getElementById('diseaseList');
+
+viewMoreLink.addEventListener('click', function(e) {
+  e.preventDefault();
+  diseaseList.classList.toggle('show');
+
+  // Toggle button text
+  if(diseaseList.classList.contains('show')){
+    viewMoreLink.textContent = 'View Less';
+  } else {
+    viewMoreLink.textContent = 'View More';
+  }
+});
+
+
